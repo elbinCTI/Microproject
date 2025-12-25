0bd1098129fe85e30c4977dd2211fe86a28a6969
elbinCTI <elbin2006@gmail.com>
2025-12-25 23:58:27 +0530

    UPDATE 1CHP.py

--- 1CHP.py
+++ 1CHP.py
@@ -1,7 +1,7 @@
 import csv as c
 a=input("enter text to decode: ")
 p=1
-with open("C:\\Users\\elbin\\OneDrive\\Desktop\\hc\\ds2.csv", mode="r") as f:
+with open("ds2.csv", mode="r") as f:
     reader=c.reader(f)  
     next(reader) 
     for i in a:
@@ -16,3 +16,4 @@ with open("C:\\Users\\elbin\\OneDrive\\Desktop\\hc\\ds2.csv", mode="r") as f:
             p=1
         f.seek(0)
         next(reader)
+
